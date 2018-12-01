538f79408062a3a301a934de9bf4506c76c4f8b8
Quan Trinh <qutrinh@.42.fr>
2018-12-01 22:24:51 +0100

    almost done

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,27 @@
 /*   By: qutrinh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 13:29:38 by qutrinh           #+#    #+#             */
-/*   Updated: 2018/12/01 19:42:01 by qutrinh          ###   ########.fr       */
+/*   Updated: 2018/12/01 22:23:59 by qutrinh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_list	get_current_file(const int fd, t_list **files)
+t_list	*get_current_file(const int fd, t_list **files)
 {
 	t_list			*tmp;
 
 	tmp = *files;
-	while(tmp)
+	while (tmp)
 	{
 		if ((int)tmp->content_size == fd)
 			return (tmp);
 		tmp = tmp->next;
 	}
-	tmp = lstnew(NULL, fd);
+	tmp = ft_lstnew("\0", fd);
 	ft_lstadd(files, tmp);
-	return (*files);
+	tmp = *files;
+	return (tmp);
 }
 
 int		copy_line(char *str, char **line)
@@ -33,33 +34,56 @@ int		copy_line(char *str, char **line)
 	int				i;
 
 	i = 0;
-	while(str[i++])
+	while(str[i])
+	{
 		if(str[i] == '\n')
 			break;
-	*line = ft_strnew(i + 1);
+		i++;
+	}
+	*line = ft_strnew(i);
 	*line = ft_strncpy(*line, str, i);
 	return (i);
+}
+
+t_list	*del_str(t_list *current, int i)
+{
+	char			*tmp;
+
+	if (i < (int)ft_strlen(current->content))
+	{
+		tmp = current->content;
+		current->content = ft_strdup(current->content + i + 1);
+		ft_strdel(&tmp);
+	}
+	else
+		ft_strclr(current->content);
+	return (current);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	static t_list	*files;
 	t_list			*current;
-	char			tmp[BUFF_SIZE ++ 1];
+	char			tmp[BUFF_SIZE + 1];
+	char			*tmp2;
+	int				i;
 
 	if ((fd < 0 || line == NULL || read(fd, tmp, 0) < 0))
 		return (-1);
 	current = get_current_file(fd, &files);
-	ft_bzero(tmp1);
 	while ((i = read(fd, tmp, BUFF_SIZE)))
 	{
+		tmp[i] = 0;
+		tmp2 = current->content;
 		current->content = ft_strjoin(current->content, tmp);
+		ft_strdel(&tmp2);
 		if (ft_strchr(tmp, '\n'))
 			break;
 	}
 	if (i < BUFF_SIZE && !ft_strlen(current->content))
 		return (0);
 	i = copy_line(current->content, line);
+	current = del_str(current, i);
 	return (1);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,22 @@
 /*   By: qutrinh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 13:26:25 by qutrinh           #+#    #+#             */
-/*   Updated: 2018/11/26 17:35:41 by qutrinh          ###   ########.fr       */
+/*   Updated: 2018/12/01 22:11:53 by qutrinh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 42
+# define BUFF_SIZE 1
 
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
 int			get_next_line(const int fd, char **line);
 
