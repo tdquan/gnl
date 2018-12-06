4f9f7b98433d3c4ed10ad1d243ede1b0b6a42e65
Quan Trinh <qutrinh@.42.fr>
2018-12-06 18:25:55 +0100

    fixed inefficient ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: qutrinh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/24 18:33:30 by qutrinh           #+#    #+#             */
-/*   Updated: 2018/11/24 18:33:37 by qutrinh          ###   ########.fr       */
+/*   Updated: 2018/12/06 18:19:58 by qutrinh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
+	size_t	len;
 
 	i = 0;
-	while (i < ft_strlen((char *)s) + 1)
+	len = ft_strlen((char *)s);
+	while (i < len + 1)
 	{
 		if (*(s + i) == (char)c)
 			return ((char *)s + i);
